ba9200534251a5d3b4d66838c6d450ea9cc90f0c
kelvinhassan <ngerajustin@gmail.com>
2023-01-25 07:39:09 +0000

    get response

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,3 @@
-export default function getResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'success',
-      });
-    }
-      reject(new Error('The fake API is not working currently'));
-  });
+export default function getResponseFromAPI() {
+	  return new Promise(() => {});
 }
